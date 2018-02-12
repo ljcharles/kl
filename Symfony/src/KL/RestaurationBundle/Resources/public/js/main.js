4d524ac1efe659034c3e15921d6aad19aa2fa577
freshloic <loicjeancharles@gmail.com>
2018-02-12 16:13:25 -0400

    Assign active class to navbar based on current page URL with jQuery

--- Symfony/src/KL/RestaurationBundle/Resources/public/js/main.js
+++ Symfony/src/KL/RestaurationBundle/Resources/public/js/main.js
@@ -3,5 +3,10 @@ $(document).ready(function () {
     $('html, body').animate({
       scrollTop: $("#about").offset().top
     }, 1000)
-  })
+  });
+
+  // get current URL path and assign 'active' class
+	var pathname = window.location.pathname;
+  $('.nav > li > a').parent().removeClass("active"); 
+  $('.nav > li > a[href="'+pathname+'"]').parent().addClass('active');
 });
