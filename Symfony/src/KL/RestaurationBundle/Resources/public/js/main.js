3b93af1dca97cd963569c391730b8146661dae81
freshloic <loicjeancharles@gmail.com>
2018-02-13 18:11:08 -0400

    design error 404 page

--- Symfony/src/KL/RestaurationBundle/Resources/public/js/main.js
+++ Symfony/src/KL/RestaurationBundle/Resources/public/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   chevronAction();
   activeAction();
+  hideMdpAction();
 });
 
 function activeAction() {
@@ -14,5 +15,28 @@ function chevronAction() {
     $('html, body').animate({
       scrollTop: $("#about").offset().top
     }, 1000);
+  });
+}
+
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    // Personnaliser vos coordonnées ici latitudepuis longitude
+    center: new google.maps.LatLng(16.1547308, -61.9560547),
+    zoom: 10,
+    disableDefaultUI: true
+  }
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+
+function hideMdpAction() {
+  $('.show-password').click(function() {
+    if($(this).prev('input').prop('type') == 'password') {
+      $(this).prev('input').prop('type','text');
+      $(this).html('<i class="fa fa-eye-slash doree"></i>');
+    } else {
+      $(this).prev('input').prop('type','password');
+      $(this).html('<i class="fa fa-eye doree"></i>');
+    }
   });
 }
