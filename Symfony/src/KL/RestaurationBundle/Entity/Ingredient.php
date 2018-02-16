a8afc4a9fe58f44248ec61063add09d154961241
freshloic <loicjeancharles@gmail.com>
2018-02-16 16:12:45 -0400

    generate entities

--- Symfony/src/KL/RestaurationBundle/Entity/Ingredient.php
+++ Symfony/src/KL/RestaurationBundle/Entity/Ingredient.php
@@ -1,10 +1,11 @@
 <?php
+namespace KL\RestaurationBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
 * IngredientController
 *
-*@ORM\Table(name=kl_ingredient")
+*@ORM\Table(name="kl_ingredient")
 *@ORM\Entity(repositoryClass="KL\RestaurationBundle\Repository\IngredientRepository")
 */
 
@@ -16,14 +17,14 @@ class Ingredient
   *
   *@ORM\Column(name="id",type="integer")
   *@ORM\Id
-  *@ORM\GeneratedValue(strategy=AUTO)
+  *@ORM\GeneratedValue(strategy="AUTO")
   */
   protected $id;
 
   /**
   *@var string
   *
-  *@ORM\Column(name="nom",type="string",lenght=255)
+  *@ORM\Column(name="nom",type="string",length=255)
   */
   protected $nom;
 
@@ -35,10 +36,90 @@ class Ingredient
   protected $quantite;
 
   /**
-  *@ORM\ManytoOne(targetEntity="KL\RestaurationBundle\Entity\Produit",inverseBy="ingredients")
+  *@ORM\ManyToOne(targetEntity="KL\RestaurationBundle\Entity\Produit",inversedBy="ingredients")
   *@ORM\JoinColumn()
   */
   private $produit;
-}
 
-?>
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Ingredient
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set quantite
+     *
+     * @param integer $quantite
+     *
+     * @return Ingredient
+     */
+    public function setQuantite($quantite)
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+
+    /**
+     * Get quantite
+     *
+     * @return integer
+     */
+    public function getQuantite()
+    {
+        return $this->quantite;
+    }
+
+    /**
+     * Set produit
+     *
+     * @param \KL\RestaurationBundle\Entity\Produit $produit
+     *
+     * @return Ingredient
+     */
+    public function setProduit(\KL\RestaurationBundle\Entity\Produit $produit = null)
+    {
+        $this->produit = $produit;
+
+        return $this;
+    }
+
+    /**
+     * Get produit
+     *
+     * @return \KL\RestaurationBundle\Entity\Produit
+     */
+    public function getProduit()
+    {
+        return $this->produit;
+    }
+}
--- Symfony/src/KL/RestaurationBundle/Entity/Produit.php
+++ Symfony/src/KL/RestaurationBundle/Entity/Produit.php
@@ -1,10 +1,12 @@
 <?php
+
+namespace KL\RestaurationBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
 * ProduitController
 *
-*@ORM\Table(name=kl_produit")
+*@ORM\Table(name="kl_produit")
 *@ORM\Entity(repositoryClass="KL\RestaurationBundle\Repository\ProduitRepository")
 */
 
@@ -16,14 +18,14 @@ class Produit
   *
   *@ORM\Column(name="id",type="integer")
   *@ORM\Id
-  *@ORM\GeneratedValue(strategy=AUTO)
+  *@ORM\GeneratedValue(strategy="AUTO")
   */
   private $id;
 
   /**
   *@var string
   *
-  *@ORM\Column(name="nom",type="string",lenght=255)
+  *@ORM\Column(name="nom",type="string",length=255)
   */
   private $nom;
 
@@ -61,7 +63,7 @@ class Produit
   private $ingredients;
 
   /**
-  *@ORM\ManytoOne(targetEntity="KL\RestaurationBundle\Entity\GammeProduit",inverseBy="produits")
+  *@ORM\ManyToOne(targetEntity="KL\RestaurationBundle\Entity\GammeProduit",inversedBy="produits")
   *@ORM\JoinColumn()
   */
   private $gammeProduit;
@@ -70,6 +72,192 @@ class Produit
   {
     $this->ingredients= new ArrayCollection();
   }
-}
 
-?>
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Produit
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prix
+     *
+     * @param string $prix
+     *
+     * @return Produit
+     */
+    public function setPrix($prix)
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    /**
+     * Get prix
+     *
+     * @return string
+     */
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Produit
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set note
+     *
+     * @param integer $note
+     *
+     * @return Produit
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    /**
+     * Get note
+     *
+     * @return integer
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     *
+     * @return Produit
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Add ingredient
+     *
+     * @param \KL\RestaurationBundle\Entity\Ingredient $ingredient
+     *
+     * @return Produit
+     */
+    public function addIngredient(\KL\RestaurationBundle\Entity\Ingredient $ingredient)
+    {
+        $this->ingredients[] = $ingredient;
+
+        return $this;
+    }
+
+    /**
+     * Remove ingredient
+     *
+     * @param \KL\RestaurationBundle\Entity\Ingredient $ingredient
+     */
+    public function removeIngredient(\KL\RestaurationBundle\Entity\Ingredient $ingredient)
+    {
+        $this->ingredients->removeElement($ingredient);
+    }
+
+    /**
+     * Get ingredients
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getIngredients()
+    {
+        return $this->ingredients;
+    }
+
+    /**
+     * Set gammeProduit
+     *
+     * @param \KL\RestaurationBundle\Entity\GammeProduit $gammeProduit
+     *
+     * @return Produit
+     */
+    public function setGammeProduit(\KL\RestaurationBundle\Entity\GammeProduit $gammeProduit = null)
+    {
+        $this->gammeProduit = $gammeProduit;
+
+        return $this;
+    }
+
+    /**
+     * Get gammeProduit
+     *
+     * @return \KL\RestaurationBundle\Entity\GammeProduit
+     */
+    public function getGammeProduit()
+    {
+        return $this->gammeProduit;
+    }
+}
