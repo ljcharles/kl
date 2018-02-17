e9e7a70c2d37711278a4c7741eb31b0bee98f1f3
Karenkz <karen971.kz@gmail.com>
2018-02-16 23:20:52 -0400

    entité user, repository, ajout vu index et vu d 'ingredient

---
+++ Symfony/src/KL/RestaurationBundle/Controller/IngredientController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace KL\RestaurationBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class IngredientController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('KLRestaurationBundle:Ingredient:index.html.twig', array(
+            // ...
+        ));
+    }
+
+    public function viewAction()
+    {
+        return $this->render('KLRestaurationBundle:Ingredient:view.html.twig', array(
+            // ...
+        ));
+    }
+
+    public function editAction()
+    {
+        return $this->render('KLRestaurationBundle:Ingredient:edit.html.twig', array(
+            // ...
+        ));
+    }
+
+    public function addAction()
+    {
+        return $this->render('KLRestaurationBundle:Ingredient:add.html.twig', array(
+            // ...
+        ));
+    }
+
+    public function deleteAction()
+    {
+        return $this->render('KLRestaurationBundle:Ingredient:delete.html.twig', array(
+            // ...
+        ));
+    }
