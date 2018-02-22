0d21b008ae14487ccd1f3d39e443af12fd5c63a8
Karenkz <karen971.kz@gmail.com>
2018-02-22 11:05:22 -0400

    entité commande & repository  lien commande produit, suppr entite panier

---
+++ Symfony/src/KL/RestaurationBundle/Entity/Commande.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace KL\RestaurationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Commande
+ *
+ * @ORM\Table(name="commande")
+ * @ORM\Entity(repositoryClass="KL\RestaurationBundle\Repository\CommandeRepository")
+ */
+class Commande
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+    * @ORM\OneToMany(targetEntity="KL\RestaurationBundle\Entity\Produit", mappedBy="commande", cascade={"all"})
+    */
+    private $produits;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->produits = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Add produit
+     *
+     * @param \KL\RestaurationBundle\Entity\Produit $produit
+     *
+     * @return Commande
+     */
+    public function addProduit(\KL\RestaurationBundle\Entity\Produit $produit)
+    {
+        $this->produits[] = $produit;
+
+        return $this;
+    }
+
+    /**
+     * Remove produit
+     *
+     * @param \KL\RestaurationBundle\Entity\Produit $produit
+     */
+    public function removeProduit(\KL\RestaurationBundle\Entity\Produit $produit)
+    {
+        $this->produits->removeElement($produit);
+    }
+
+    /**
+     * Get produits
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getProduits()
+    {
+        return $this->produits;
+    }
+}
+?>
